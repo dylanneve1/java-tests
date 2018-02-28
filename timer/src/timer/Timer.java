f0ff013bb67d3db7674fb71bdd14fdabbdf4895a
Dylan Neve <dylanneve1@users.noreply.github.com>
2018-02-28 09:17:33 +0000

    Add timer

---
+++ timer/src/timer/Timer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timer;
+
+/**
+ *
+ * @author dylan
+ */
+public class Timer {
+    int numberOfPresses;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.setSize(300,300);
+        obj.setVisible(true);
+    }
+    
+}
